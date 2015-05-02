41d29d5ac351f6653d0bd7a5cbd367b35bcfe872
Marc Bluemner <marc.bluemner@gmail.com>
2015-05-02 23:46:45 +0200

    fixed friends

--- app/controllers/friends/base.js
+++ app/controllers/friends/base.js
@@ -1,36 +1,31 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-    isValid: Ember.computed(
-                 'model.email',
-                 'model.firstName',
-                 'model.lastName',
-                 'model.twitter',
-                 function() {
-                     return
-                         !Ember.isEmpty(this.get('model.email')) &&
-                         !Ember.isEmpty(this.get('model.firstName')) &&
-                         !Ember.isEmpty(this.get('model.lastName')) &&
-                         !Ember.isEmpty(this.get('model.twitter'));
-                 }
-                 ),
-    actions: {
-        save: function() {
-            if (this.get('isValid')) {
-                var _this = this;
-
-                this.get('model').save().then(function(friend) {
-                    _this.transitionToRoute('friends.show', friend);
-                });
-            } else {
-                this.set('errorMessage', 'You have to fill all the fields'); 
-            }
-            return false;
-        },
-        cancel: function() {
-            this.transitionToRoute('friends');
-
-            return true;
-        }
+export default Ember.ObjectController.extend({
+  isValid: Ember.computed(
+    'email',
+    'firstName',
+    'lastName',
+    'twitter',
+    function() {
+      return !Ember.isEmpty(this.get('email')) &&
+        !Ember.isEmpty(this.get('firstName')) &&
+        !Ember.isEmpty(this.get('lastName')) &&
+        !Ember.isEmpty(this.get('twitter'));
     }
+  ),
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        var _this = this;
+        this.get('model').save().then(function(friend) {
+          _this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+    },
+    cancel: function() {
+      return true;
+    }
+  }
 });
--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         cancel: function() {
-            this.transitionToRoute('friends');
-
+            this.transitionToRoute('friends.show', this);
             return false;
         }
     }
